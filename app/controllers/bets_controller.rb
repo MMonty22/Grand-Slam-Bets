cd75ecc4e2f74169deca595495bfd2654a557dcc
Michael <michael.c.montgomery@gmail.com>
2023-02-13 18:13:43 -0600

    added actions to bets controller

--- app/controllers/bets_controller.rb
+++ app/controllers/bets_controller.rb
@@ -1,2 +1,37 @@
 class BetsController < ApplicationController
+    def index
+        bets = Bet.all
+        render json: bets, status: :created
+    end
+
+    def create
+        bet = @current_user.bets.create(bet_params)
+        if bet.valid?
+            render json: bet, status: :created
+        else
+            render json: {errors: bet.errors.full_messages}, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        bet = @current_user.bets.find_by(id: params[:id])
+        bet.update!(bet_params)
+        render json: bet
+    end
+
+    def destroy
+        bet = @current_user.bets.find_by(id: params[:id])
+        if bet
+            bet.destroy
+            head :no_content
+        else
+            render json: {error: "Bet not found"}, status: :not_found
+        end
+    end
+
+    private
+
+    def bet_params
+        params.permit(:game_id, :category, :description, :odds)
+    end
 end
