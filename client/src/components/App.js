26eb5736294e9bdb57f564d2012311ae22ed6a8e
Michael <michael.c.montgomery@gmail.com>
2023-02-14 09:28:46 -0600

    set up home component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,6 +4,7 @@ import { UserProvider } from '../Context/UserContext';
 import SignUp from './SignUp';
 import Login from './Login';
 import NavBar from './NavBar'
+import Home from './Home';
 
 function App() {
   const [username, setUsername] = useState("")
@@ -16,6 +17,7 @@ function App() {
       <UserProvider>
       <NavBar />
       <Routes>
+          <Route exact path="/" element={<Home />} />
           <Route exact path="/signup" element={<SignUp username={username} setUsername={setUsername} password={password} setPassword={setPassword} passwordConfirmation={passwordConfirmation} setPasswordConfirmation={setPasswordConfirmation} errors={errors} setErrors={setErrors}/>}/>
           <Route exact path="/login" element={<Login username={username} setUsername={setUsername} password={password} setPassword={setPassword} passwordConfirmation={passwordConfirmation} setPasswordConfirmation={setPasswordConfirmation} errors={errors} setErrors={setErrors}/>} />
         </Routes>
---
+++ client/src/components/Home.js
@@ -0,0 +1,81 @@
+import React, { useState, useContext} from 'react'
+import { useNavigate } from "react-router-dom"
+import { UserContext } from '../Context/UserContext';
+
+function Home() {
+    const navigate = useNavigate()
+    const { logout, state, dispatch } = useContext(UserContext)
+    const [showBets, setShowBets] = useState(false)
+    const [showComments, setShowComments] = useState(false)
+    //console.log('state', state)
+
+    const userBets = state?.user?.bets?.map((bet) => {
+        return <ul key={bet.id}>
+            <li>Bet: {bet.description}</li>
+            <li>Odds: {bet.odds}</li>
+    </ul>})
+
+    const userComments = state.user?.comments?.map((com) => <ul key={com.id}>
+        <li>{com.text}</li>
+    </ul>)
+
+    function handleShowBets() {
+        setShowBets(!showBets)
+    }
+
+    function handleShowComments() {
+        setShowComments(!showComments)
+    }
+
+    function handleUserLogout() {
+        fetch('/logout', {
+            method: 'DELETE',
+            headers: { "Content-Type": "application/json" },
+        })
+            .then(() => {
+                logout()
+                navigate('/')
+            })
+    }
+
+    if (state.initialLoad) {
+        return <h3 id='loading'>"Loading..."</h3>
+    }
+    else if (state.loggedIn && showBets)
+        return (
+        <div className='home'>
+            <h2>Welcome, {state.user.username}</h2>
+            <button className='logoutButton' onClick={handleUserLogout}>Logout</button>
+            <br />
+            <button onClick={handleShowBets}>{showBets ? "Hide My Bets" : "Show My Bets"}</button>
+            <br />
+            <button onClick={handleShowComments}>{showComments ? "Hide My Comments" : "Show My Comments"}</button>
+            {userBets}
+        </div>
+    )
+    else if (state.loggedIn && showComments)
+        return (
+        <div className='home'>
+            <h2>Welcome, {state.user.username}</h2>
+            <button className='logoutButton' onClick={handleUserLogout}>Logout</button>
+            <br />
+            <button onClick={handleShowBets}>{showBets ? "Hide My Bets" : "Show My Bets"}</button>
+            <br />
+            <button onClick={handleShowComments}>{showComments ? "Hide My Comments" : "Show My Comments"}</button>
+            {userComments}
+        </div>
+    )
+    else if (state.loggedIn)
+        return (
+            <div className='home'>
+                <h2>Welcome, {state.user.username}</h2>
+                <button className='logoutButton' onClick={handleUserLogout}>Logout</button>
+                <br />
+                <button onClick={handleShowBets}>{showBets ? "Hide My Bets" : "Show My Bets"}</button>
+                <br />
+                <button onClick={handleShowComments}>{showComments ? "Hide My Comments" : "Show My Comments"}</button>
+            </div>
+        )
+}
+
+export default Home
