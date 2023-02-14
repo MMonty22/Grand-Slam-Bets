06b6de0351480da6a7354fa78a0cf1a3bb81cbb0
Michael <michael.c.montgomery@gmail.com>
2023-02-13 18:15:52 -0600

    added actions to games controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,23 @@
 class GamesController < ApplicationController
+    skip_before_action :authorize, only: :index
+    
+    def index
+        games = Game.all
+        render json: games, status: :created
+    end
+
+    def create
+        game = Game.create(game_params)
+        if game.valid?
+            render json: game, status: :created
+        else
+            render json: {errors: game.errors.full_messages}, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def game_params
+        params.permit(:away_team, :home_team)
+    end
 end
